74a78f2d9acddc5116e87a8c9a7714c5307f2572
LeSGoel <goelshobhit34@gmail.com>
2018-07-13 11:06:11 +0530

    ADD:dependencies

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -17,9 +17,31 @@ import messages from './messages';
 export default class HomePage extends React.PureComponent {
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+        <form className="my-form my-form--theme-dark my-form--compact">
+          <div
+            className="my-form__input-area"
+            placeholder="Enter your username"
+          >
+            Username:
+            <input id="username" className="my-form__input" type="text" />
+          </div>
+          <div
+            className="my-form__input-area"
+            placeholder="enter your password"
+          >
+            Password:
+            <input id="password" className="my-form__input" type="password" />
+          </div>
+          <button
+            type="submit"
+            className="my-form__submit my-form__submit--disabled"
+          />
+        </form>
+      </div>
     );
   }
 }
